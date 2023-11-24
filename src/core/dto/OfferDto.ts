811095081ba1020c32de02d1d341c7d0ddd8438c
zhamdi <zied@weally.org>
2023-11-24 14:30:21 +0100

    Changed default values for OfferDto.ts

--- src/core/dto/OfferDto.ts
+++ src/core/dto/OfferDto.ts
@@ -3,10 +3,10 @@ import type { IMinimalId, IOffer } from "@user-credits/core";
 import { EntityDto } from "./EntityDto";
 
 export class OfferDto<K extends IMinimalId> extends EntityDto<K, IOffer<K>> {
-  description: string = "-";
+  description: string = "";
   advantages: Map<string, string> = new Map<string, string>();
-  callToAction: string = "-";
-  highlightingMessage: string = "-";
+  callToAction: string = "";
+  highlightingMessage: string = "";
   constructor(offer: IOffer<K>) {
     super(offer);
   }
